912915d35257fc8914afbcb89a5efc455d3f21e1
junmox8 <931097192@qq.com>
2022-12-09 01:05:32 +0800

    12-9-endd

--- src/components/album/index.js
+++ src/components/album/index.js
@@ -3,7 +3,9 @@ import style from "./index.module.scss";
 import { PlayCircleOutlined } from "@ant-design/icons";
 import Song from "../song";
 import { getAlbumDetail } from "../../axios/service/music";
-export default function AlbumComponent(props) {
+import { connect } from "react-redux";
+import PlayAllMusic from "../../utils/playAllMusic";
+function AlbumComponent(props) {
   useEffect(() => {
     (async function () {
       const {
@@ -13,6 +15,9 @@ export default function AlbumComponent(props) {
     })();
   }, []);
   const [songs, setSongs] = useState([]);
+  const playAllSongs = async () => {
+    PlayAllMusic(songs, props.userInfo.isLogin);
+  };
   return (
     <div className={style.main}>
       <div className={style.hotContent}>
@@ -24,7 +29,7 @@ export default function AlbumComponent(props) {
           <div className={style.firstLine}>
             <div className={style.text}>{props.name}</div>
             <PlayCircleOutlined
-              // onClick={playAllSongs}
+              onClick={playAllSongs}
               style={{ marginLeft: "25px", cursor: "pointer" }}
             ></PlayCircleOutlined>
           </div>
@@ -47,3 +52,10 @@ export default function AlbumComponent(props) {
     </div>
   );
 }
+const a = (state) => {
+  return {
+    userInfo: state.userInfo,
+  };
+};
+const b = null;
+export default connect(a, b)(AlbumComponent);
--- src/pages/singerDetail/detail/index.js
+++ src/pages/singerDetail/detail/index.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getSingerDesc } from "../../../axios/service/singers";
+import { useSearchParams } from "react-router-dom";
 import style from "./index.module.scss";
-export default function Detail() {
-  return <div>index</div>;
+export default function Detail(props) {
+  useEffect(() => {
+    (async function () {
+      const result = await getSingerDesc(singerId);
+      console.log(result);
+    })();
+  }, []);
+  const [getParmas, setParmas] = useSearchParams();
+  const singerId = getParmas.get("id");
+  return <div className={style.main}>index</div>;
 }
