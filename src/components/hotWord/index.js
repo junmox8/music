aa782fe112952cc56e3655249440e07ef43b5df7
junmox8 <931097192@qq.com>
2023-01-05 16:05:03 +0800

    1-5-3

--- src/components/hotWord/index.js
+++ src/components/hotWord/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import style from "./index.module.scss";
+import { useNavigate } from "react-router-dom";
 export default function HotWord(props) {
+  const navigate = useNavigate();
   return (
-    <div className={style.main}>
+    <div
+      className={style.main}
+      onClick={() => {
+        navigate("/searchPage/song?words=" + props.word);
+      }}
+    >
       <div
         className={style.xuhao}
         style={{ color: props.index <= 3 ? "#fc5555" : "#cfcfcf" }}
